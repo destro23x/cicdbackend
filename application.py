04f97698c414b44c590b740b88517f542e6193e7
unknown <fristajl.pl.inc@gmail.com>
2024-04-19 17:35:50 +0200

    Second commit

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 import logging.handlers
+import boto3
+import base64
+from botocore.exceptions import ClientError
 
 # Create logger
 logger = logging.getLogger(__name__)
@@ -20,6 +23,56 @@ logger.addHandler(handler)
 
 welcome = """{'info': 'hello world'}"""
 
+def get_secret():
+
+    secret_name = "pjatk/secret"
+    region_name = "eu-west-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+    return json.loads(secret)  # returns the secret as dictionary
 
 def application(environ, start_response):
     path = environ['PATH_INFO']
@@ -37,7 +90,9 @@ def application(environ, start_response):
             logger.warning('Error retrieving request body for async work.')
         response = ''
     else:
-        response = welcome
+        secret_value = get_secret()
+        response = secret_value
+        #response = welcome
     start_response("200 OK", [
         ("Content-Type", "text/html"),
         ("Content-Length", str(len(response)))
